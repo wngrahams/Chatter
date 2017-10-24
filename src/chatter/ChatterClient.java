fcd8c2925dbb1896163ced5fb76ea464f078a64b
wngrahams <wgs11@georgetown.edu>
2017-10-24 13:31:57 -0400

    Added ChatterClient and ChatterServer classes

---
+++ src/chatter/ChatterClient.java
@@ -0,0 +1,10 @@
+package chatter;
+
+public class ChatterClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/chatter/ChatterServer.java
@@ -0,0 +1,10 @@
+package chatter;
+
+public class ChatterServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
