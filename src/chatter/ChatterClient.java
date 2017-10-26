f40f83514ea5c75173f1fdf6815f035fced98577
wngrahams <wgs11@georgetown.edu>
2017-10-25 21:00:55 -0400

    Send objects to server on connection and on message typed

--- src/chatter/ChatterClient.java
+++ src/chatter/ChatterClient.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
@@ -41,6 +42,9 @@ public class ChatterClient {
 	public ChatterClient(String ip, int port) {
 		clientFrame = new ClientFrame(this);
 		clientUser = new User();
+		clientFrame.addNewUser(getUser());
+		clientFrame.addNewUser(new User("graham", "localhost"));
+		clientFrame.addNewUser(new User("Jim", "localhost"));
 	   
 		serverIP = ip;
 		serverPort = port;
@@ -52,6 +56,8 @@ public class ChatterClient {
 		try {
 			socket = new Socket(serverIP, serverPort);
 			System.out.println("Connecting to server...");
+			ObjectOutputStream userToServer = new ObjectOutputStream(socket.getOutputStream());
+			userToServer.writeObject(this);
 		} catch (IOException e) {
 			System.err.println("Failed to connect to server.");
 			System.err.println(e);
@@ -60,5 +66,15 @@ public class ChatterClient {
 	
 	public User getUser() {
 		return clientUser;
+	}
+	
+	public void sendToServer(String text, User recipient) {
+		try {
+			ObjectOutputStream userToServer = new ObjectOutputStream(socket.getOutputStream());
+			Message message = new Message(recipient, clientUser, text);
+			userToServer.writeObject(message);
+		} catch (IOException e) {
+			System.out.println("Error sending message to " + recipient);
+		}
 	}
 }
