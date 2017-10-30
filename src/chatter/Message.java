72c3261f5c08195c5d9b0f59736e82aa91575dcd
wngrahams <wgs11@georgetown.edu>
2017-10-30 19:23:01 -0400

    Added messageType, additional constructors

--- src/chatter/Message.java
+++ src/chatter/Message.java
@@ -14,15 +14,32 @@ public class Message implements Serializable {
 	private User recipient;
 	private User sender;
 	
+	private int messageType;
+	
+	public static final int TEXT_MESSAGE = 0b0;
+	public static final int USER_MESSAGE = 0b1;
+	
+	public Message() {
+		this("");
+	}
+	
 	public Message(String text) {
 		this(null, null, text);
 	}
+	
+	public Message(User from, int messageType) {
+		this(User.SERVER, from, from + " logged on", messageType);
+	}
 
 	public Message(User to, User from, String text) {
+		this(to, from, text, TEXT_MESSAGE);
+	}
+	
+	public Message(User to, User from, String text, int messageType){
 		setRecipient(to);
 		setSender(from);
 		setMessage(text);
-		
+		setType(messageType);
 	}
 
 	public String getMessage() {
@@ -35,7 +52,7 @@ public class Message implements Serializable {
 	
 	public User getRecipient() {
 		if (null == recipient)
-			return new User("global chat");
+			return User.SERVER;
 		
 		return recipient;
 	}
@@ -51,11 +68,22 @@ public class Message implements Serializable {
 	public void setSender(User sender) {
 		this.sender = sender;
 	}
+	
+	public int getType() {
+		return messageType;
+	}
+	
+	public void setType(int type) {
+		if (type != TEXT_MESSAGE && type != USER_MESSAGE) 
+			throw new RuntimeException("Unknown error type");
+		
+		messageType = type;
+	}
 
 	@Override
 	public String toString() {
 		String output;
-		if (sender != null)
+		if (sender != User.SERVER)
 			output = sender + ": " + message;
 		else 
 			output = "Message from server: " + message;
