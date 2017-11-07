984bce753783589668c648652e990b65d84af7d0
wngrahams <wgs11@georgetown.edu>
2017-11-06 20:38:35 -0500

    Documentation for ChatterServer

--- src/server/ChatterServer.java
+++ src/server/ChatterServer.java
@@ -10,6 +10,15 @@ import java.util.ArrayList;
 import chatter.Message;
 import chatter.User;
 
+/** 
+ * The <code>ChatterServer</code> class creates allows for
+ * the connection of multiple clients and manages their interactions,
+ * including the sending of global and private messages, name changes,
+ * logging on, and logging off.
+ * 
+ * @author Graham Stubbs (wgs11@georgetown.edu)
+ * @author Cooper Logerfo (cml264@georgetown.edu)
+ */
 public class ChatterServer {
 
 	private ServerFrame serverFrame;
@@ -17,27 +26,49 @@ public class ChatterServer {
 	private int port = 0xFFFF;
 	private boolean keepGoing;
 	
+	// An ArrayList to keep track of all connected clients
 	private ArrayList<ChatterThread> threadList;
 	
-	   
+	
+	/** 
+	 * Starts a new <code>ChatterServer</code> at the port provided,
+	 * or at the default port <code>0xFFFF</code> if no port is provided.
+	 * 
+	 * @param args Desired connection port
+	 */
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		ChatterServer cs = new ChatterServer();
 	}
 	
+	/** 
+	 * Creates a new <code>ChatterServer</code> at the default port <code>0xFFFF</code>
+	 */
 	public ChatterServer() {
 		this(0xFFFF);
 	}
 	
+	/** 
+	 * Creates a new <code>ChatterServer</code> at the given port
+	 * 
+	 * @param port Desired connection port
+	 */
 	public ChatterServer(int port)
 	{
 		this.port = port;
-		serverFrame = new ServerFrame(this);
+		serverFrame = new ServerFrame();
 		threadList = new ArrayList<ChatterThread>();
 		
 		startServer();
 	}
 	
+	/** 
+	 * Checks to see if a given username is currently in use by a connected
+	 * client
+	 * 
+	 * @param name The name to check
+	 * @return <code>true</code> if the name is available, <code>false</code> otherwise
+	 */
 	private boolean checkNameAvailable(String name) {
 		synchronized (threadList) {
 			for (int i=0; i<threadList.size(); i++) {
@@ -51,6 +82,13 @@ public class ChatterServer {
 		}
 	}
 	
+	/** 
+	 * Called when a client first connects to the server, sends a log-on
+	 * <code>Message</code> to the new client for every other connected
+	 * client, allowing for the new client to populate its list of users
+	 * 
+	 * @param reciever The newly connected <code>ChatterThread</code> to receive the <code>Message</code>s
+	 */
 	private void getAllUsers(ChatterThread reciever) {
 		synchronized(threadList) {
 			for (int i=0; i<threadList.size(); i++) {
@@ -62,21 +100,31 @@ public class ChatterServer {
 		}
 	}
 	
+	/** 
+	 * Removes a given client from the list of clients
+	 * 
+	 * @param client The client to remove
+	 */
 	private void removeClientFromList(ChatterThread client) {
 		synchronized(threadList) {
 			threadList.remove(this);
 		}
-		
-		
 	}
 	
+	/** 
+	 * Sends a given <code>Message</code> to connected clients. The specific 
+	 * clients the <code>Message</code> is sent to depends on the sender and
+	 * the type of <code>Message</code>
+	 * 
+	 * @param m The <code>Message</code> to send
+	 */
 	private void sendMessageToClient(Message m) {
 		synchronized(threadList) {
 			if (m.getRecipient() == User.SERVER) {
-				// Send message to all clients
+				// The message is global, send to all clients
 				for (int i=0; i<threadList.size(); i++) {
-					ChatterThread clientRecipient = threadList.get(i);
 					
+					ChatterThread clientRecipient = threadList.get(i);
 					if (!clientRecipient.sendMessage(m)) {
 						threadList.remove(i);
 						i--;
@@ -84,14 +132,12 @@ public class ChatterServer {
 				}
 			}
 			else {
-				System.out.println("Sending private message from " + m.getSender() + " to " + m.getRecipient());
+				// Otherwise, the message is a private message, only send it to the sender and receiver
 				for (int i=0; i< threadList.size(); i++) {
 					ChatterThread clientRecipient = threadList.get(i);
-					System.out.println("User "+ i + ": " + clientRecipient.getUser());
 					User testUser = clientRecipient.getUser();
 					
 					if (testUser.equals(m.getRecipient()) || testUser.equals(m.getSender())) {
-						System.out.println("sending...");
 						if (!clientRecipient.sendMessage(m)) {
 							threadList.remove(i);
 							i--;
@@ -102,17 +148,26 @@ public class ChatterServer {
 		}
 	}
 	
+	/** 
+	 * Creates a new <code>ServerSocket</code> at the port given on class
+	 * creation. Then creates a new <code>Socket</code> to continually
+	 * listen for new client connections. Terminates on <code>ServerFrame</code>
+	 * close or if an error is encountered when creating the <code>ServerSocket</code>
+	 */
 	private void startServer() {
 		keepGoing = true;
 				
 		try {
+			// Create the ServerSocket 
 			ServerSocket serverSocket = new ServerSocket(port);
 			serverFrame.displayMessage(new Message("Successfully started server. IP: " + serverSocket.getInetAddress() + " Port: " + port));
 			serverFrame.displayMessage(new Message("Ready to receive clients..."));
 			
+			// Do this until ServerFrame close, won't start if ServerSocket creation fails
 			while (keepGoing) {
 				
 				try {
+					// Create Socket to continually listen for new client connections
 					Socket clientSocket = serverSocket.accept();
 					
 					if (!keepGoing)
@@ -143,6 +198,16 @@ public class ChatterServer {
 		
 	}
 	
+	/** 
+	 * The <code>ChatterThread</code> class extends <code>Thread</code>
+	 * and allows for the <code>ChatterServer</code> to connect and 
+	 * communicate with a <code>ChatterClient</code> object. Multiple
+	 * <code>ChatterThread</code>s should be created to allow for 
+	 * communication between multiple clients
+	 * 
+	 * @author Graham Stubbs (wgs11@georgetown.edu)
+	 * @author Cooper Logerfo (cml264@georgetown.edu)
+	 */
 	private class ChatterThread extends Thread {
 		
 		private Socket clientSocket;
@@ -153,6 +218,14 @@ public class ChatterServer {
 		
 		private Message clientMessage;
 		
+		/** 
+		 * Creates a new <code>ChatterThread</code> object connected to a given
+		 * <code>Socket</code>. Sets up an <code>ObjectOutputStream</code> and an
+		 * <code>ObjectInputStream</code> to allow <code>Message</code>s to be sent
+		 * back and forth between the client and server
+		 * 
+		 * @param s The <code>Socket</code> the client is connected to
+		 */
 		public ChatterThread(Socket s) {
 			clientSocket = s;
 			
@@ -179,6 +252,10 @@ public class ChatterServer {
 			getAllUsers(this);
 		}
 		
+		/** 
+		 * Disconnects the client from its connected <code>ObjectOutputStream</code>
+		 * and <code>ObjectInputStream</code>. Closes the connected <code>Socket</code>.
+		 */
 		protected void close() {
 			if (send != null) {
 				try {
@@ -204,6 +281,13 @@ public class ChatterServer {
 			}
 		}
 		
+		/** 
+		 * Overrides <code>Thread.run()</code>. Continually checks for 
+		 * <code>Message</code>s sent by the connected client. If a message
+		 * is received, <code>sendMessageToClient()</code> is called to
+		 * handle the message.
+		 */
+		@Override
 		public void run() {
 			boolean clientRun = true;
 			while (clientRun) {
@@ -229,10 +313,22 @@ public class ChatterServer {
 			close();
 		}
 		
+		/** 
+		 * Returns the <code>User</code> associated with this client
+		 * 
+		 * @return The <code>User</code> associated with this client
+		 */
 		public User getUser() {
 			return clientUser;
 		}
 		
+		/** 
+		 * Uses the client's <code>ObjectOutputStream</code> to write the 
+		 * given <code>Message</code> over the network to the client
+		 * 
+		 * @param m The <code>Message</code> to be written
+		 * @return <code>true</code> if the message is successfully sent, <code>false</code> otherwise
+		 */
 		public boolean sendMessage(Message m) {
 			if (!clientSocket.isConnected() || clientSocket.isClosed()) {
 				close();
@@ -246,9 +342,8 @@ public class ChatterServer {
 				else if (m.getType() == Message.USER_NAME_MESSAGE) {
 					send.writeUnshared(m);
 					
-					// change name only for user that sent this name change request
+					// Change name only for user that sent this name change request
 					if (clientUser.equals(m.getSender())) {
-						String oldUserName = clientUser.getNickname();
 						User oldUser = new User(clientUser);
 						clientUser = new User(m.getMessage(), clientUser.getIP());
 						serverFrame.displayMessage(new Message(clientUser, oldUser, clientUser.getNickname(), Message.USER_NAME_MESSAGE));
@@ -256,6 +351,7 @@ public class ChatterServer {
 					}
 				}	
 				else if (m.getType() == Message.USER_LOGON_MESSAGE ||  m.getType() == Message.USER_LOGOFF_MESSAGE){
+					// No need to send to the user that logged on/off
 					if (m.getSender() != clientUser) 
 						send.writeUnshared(m);
 				}
